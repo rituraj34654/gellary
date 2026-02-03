ee36bc2b14f97bac801219f64f9ca268a4ee1a29
rituraj34654 <rituraj34654@gmail.com>
2026-02-03 13:47:21 +0530

    hi

--- src/main.js
+++ src/main.js
@@ -1,12 +1,49 @@
 
-const image = document.querySelectorAll('.img');
-let currentIndex = 0;
-  image.forEach((img, index)=>{
-   image.addEventListener('click',()=>{
-    currentIndex = index;
-    sh
-   })
+const lightBox = document.querySelector('.lightBox');
+const buttonNext = document.querySelector('#buttonNext');
+const buttoncut = document.querySelector('#buttoncut');
+const buttonPrev = document.querySelector('#buttonPrev');
+const showBigIMg = document.querySelector('.bigImg');
+const img = document.querySelectorAll('.img')
+
+let countImg = 0;
+
+
+
+
+img.forEach((img, index) => {
+  img.addEventListener('click',()=>{
+    countImg = index;
+  showBigIMg.src = img.src;
+  lightBox.classList.add('show');
+  })
+});
+
+buttoncut.addEventListener('click', ()=>{
+  lightBox.classList.remove('show');
+});
+
+buttonNext.addEventListener('click', ()=>{
+  countImg = (countImg+1)%img.length;
+  showBigIMg.src = img[countImg].src;
+});
+
+buttonPrev.addEventListener('click', ()=>{
+  countImg = (countImg - 1 + img.length)%img.length;
+  showBigIMg.src = img[countImg].src
 })
 
-
-
+document.addEventListener('keydown',(e)=>{
+  if(lightBox.classList.contains('show')) return;
+  if(e.key === 'ArrowRight'){
+    countImg = (countImg + 1) % img.length;
+    showBigIMg.src = img[countImg].src;
+  }
+  if(e.key === 'ArrowLeft'){
+    countImg = (countImg - 1 + img.length) % img.length;
+    showBigIMg.src = img[countImg].src;
+  }
+  if (e.key === 'Escape') {
+    lightBox.classList.remove('show');
+  }
+});
