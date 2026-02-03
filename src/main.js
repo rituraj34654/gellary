5a90d7ece19beb200cfe3a55ed4568dc7f10a764
rituraj34654 <rituraj34654@gmail.com>
2026-02-03 10:24:23 +0530

    hi

---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+const image = document.querySelectorAll('.img');
+let currentIndex = 0;
+  image.forEach((img, index)=>{
+   image.addEventListener('click',()=>{
+    currentIndex = index;
+    sh
+   })
+})
+
+
+
